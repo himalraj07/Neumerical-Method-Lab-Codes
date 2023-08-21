d1fc87ba8e61802db1384769f058d479e7f1e66e
himalraj07 <himalrajbhusal@gmail.com>
2023-08-21 14:28:25 +0545

    Euler's Method

--- 21_Eulers_Method.c
+++ 21_Eulers_Method.c
@@ -4,42 +4,48 @@
 
 #include <stdio.h>
 
-float calculateFunction(float x, float y) {
+float calculateFunction(float x, float y)
+{
     return x + y;
 }
 
-int main() {
+int main()
+{
+    printf("\nEuler's Method\n");
+    printf("==============\n\n");
+
     float initialX, initialY, stepSize, targetX;
-    
+
     // Input from the user
-    printf("Enter initial x: ");
+    printf("Enter initial x : ");
     scanf("%f", &initialX);
-    
-    printf("Enter initial y: ");
+
+    printf("Enter initial y : ");
     scanf("%f", &initialY);
-    
-    printf("Enter step size: ");
+
+    printf("Enter step size : ");
     scanf("%f", &stepSize);
-    
-    printf("Enter target x: ");
+
+    printf("Enter target x : ");
     scanf("%f", &targetX);
-    
+
     float currentX = initialX;
     float currentY = initialY;
-    
-    printf("\nEuler's Method Table:\n");
+
+    printf("\nEuler's Method Table\n");
     printf("x\t\t  y\n");
     printf("-----------------------\n");
-    
-    while (currentX <= targetX) {
+
+    while (currentX <= targetX)
+    {
         printf("%.3f\t\t%.3f\n", currentX, currentY);
-        
+
         float slope = calculateFunction(currentX, currentY);
         float deltaY = stepSize * slope;
-        
+
         currentY += deltaY;
         currentX += stepSize;
     }
-    
+    printf("\n");
     return 0;
 }

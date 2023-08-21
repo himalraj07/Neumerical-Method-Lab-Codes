89c27ee97142120538bf3b6b3c14e2eec7722760
himalraj07 <himalrajbhusal@gmail.com>
2023-08-21 14:34:06 +0545

    Heun's Method

--- 22_Heuns_Method.c
+++ 22_Heuns_Method.c
@@ -11,11 +11,20 @@ int main()
 {
     float x, y, h, xn;
 
-    printf("Heun's Method for solving ordinary differential equations\n");
-    printf("Enter initial value for x and y: ");
-    scanf("%f%f", &x, &y);
-    printf("Enter step size h and final value of x: ");
-    scanf("%f%f", &h, &xn);
+    printf("\nHeun's Method for solving ordinary differential equations\n\n");
+
+    printf("Enter initial value for x : ");
+    scanf("%f", &x);
+
+    printf("Enter initial value for y : ");
+    scanf("%f", &y);
+
+    printf("Enter step size h : ");
+    scanf("%f", &h);
+
+    printf("Enter final value of x : ");
+    scanf("%f", &xn);
+    printf("\n");
 
     while (x + h <= xn)
     {
@@ -27,6 +36,6 @@ int main()
 
         printf("At x = %f, y = %f\n", x, y);
     }
-
+    printf("\n");
     return 0;
 }
